6179567e33cf8343ff1e7756afdae154eb6871d1
djj1978 <jj_djj@sohu.com>
2022-10-18 15:55:38 +0800

    Initial commit

---
+++ src/main/java/com/passion/bot/App.java
@@ -0,0 +1,13 @@
+package com.passion.bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
